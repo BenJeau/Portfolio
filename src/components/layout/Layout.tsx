aee3cbe55630f4373d63d81a74edc83a1ab3e807
Benoit Jeaurond <benoit@jeaurond.dev>
2020-09-23 22:59:31 -0400

    Changed day.js locale based on react-intl

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,20 +1,39 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
+
+import dayjs from 'dayjs';
+import localizedFormat from 'dayjs/plugin/localizedFormat';
+import 'dayjs/locale/en-ca';
+import 'dayjs/locale/fr-ca';
+
 import Navbar from './Navbar';
 import Title from './Title';
-
 import './layout.scss';
+
+dayjs.extend(localizedFormat);
 
 interface LayoutProps {
   title: string;
   className?: string;
 }
 
-const Layout: React.FC<LayoutProps> = ({ children, title, className }) => (
-  <Fragment>
-    <Title title={title} />
-    <div className={'layout ' + (className ? className : '')}>{children}</div>
-    <Navbar />
-  </Fragment>
-);
+const Layout: React.FC<LayoutProps> = ({ children, title, className }) => {
+  if (typeof window !== 'undefined') {
+    useEffect(() => {
+      if (window.location.pathname.includes('/en/')) {
+        dayjs.locale('en-ca');
+      } else if (window.location.pathname.includes('/fr/')) {
+        dayjs.locale('fr-ca');
+      }
+    }, [window.location]);
+  }
+
+  return (
+    <Fragment>
+      <Title title={title} />
+      <div className={'layout ' + (className ? className : '')}>{children}</div>
+      <Navbar />
+    </Fragment>
+  );
+};
 
 export default Layout;
